4bb1e15caed53ae0cd86159c970b9c58f248801f
John Carlson <jawnnypoo@gmail.com>
2020-03-03 11:26:12 -0600

    We did it

--- confetti-sample/src/main/java/com/github/jinatonic/confetti/sample/EmojiBitmapFactory.kt
+++ confetti-sample/src/main/java/com/github/jinatonic/confetti/sample/EmojiBitmapFactory.kt
@@ -5,13 +5,11 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.Paint
+import android.util.TypedValue
 import android.widget.TextView
 import androidx.annotation.Px
 
 object EmojiBitmapFactory {
-
-    private val debug = true
 
     @JvmStatic
     fun create(context: Context, emoji: String, @Px size: Float): Bitmap {
@@ -23,17 +21,17 @@ object EmojiBitmapFactory {
     private class CaptureTextView internal constructor(context: Context, var size: Float) : TextView(context) {
 
         fun capture(emoji: String?): Bitmap {
-            //textSize = size
+            setTextSize(TypedValue.COMPLEX_UNIT_PX, size)
+            // Otherwise it is semi-transparent
+            setTextColor(Color.BLACK)
             text = emoji
             measure(
                 MeasureSpec.makeMeasureSpec(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED),
-                MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED))
+                MeasureSpec.makeMeasureSpec(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+            )
             layout(0, 0, measuredWidth, measuredHeight)
             val b = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
             val c = Canvas(b)
-            if (debug) {
-                c.drawColor(Color.RED)
-            }
             draw(c)
             return b
 
